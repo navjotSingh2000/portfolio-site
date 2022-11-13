476abc5d670cad15903032074059f97acbf33586
Navjot Singh <navjotsingh@Navjots-MacBook-Air.local>
2022-11-12 23:04:12 -0500

    added spring boot resume download service

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,21 @@
 import { useState } from "react";
+import Axios from "axios";
+import FileDownload from "js-file-download";
 import "../components/styles/Navbar.css";
+import Button from '@mui/material/Button';
+
+const DownloadResume = async(e) => {
+  await Axios({
+    url: "https://resume-navjot-singh.herokuapp.com",
+    method: "GET",
+    responseType: "blob"
+  }).then((res) => {
+    FileDownload(res.data, "resume-navjot-singh.pdf")
+  }).catch((error)=>{
+    console.log(error.response)
+    return error.response
+  })
+}
 
 export default function Navbar() {
     const [isNavExpanded, setIsNavExpanded] = useState(false);
@@ -36,13 +52,13 @@ export default function Navbar() {
         >
           <ul>
             <li>
-              <a href="/home">Home</a>
-            </li>
-            <li>
               <a href="/about">About</a>
             </li>
             <li>
               <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <Button onClick={()=>DownloadResume()} size="small">Download Resume</Button>
             </li>
           </ul>
         </div>
